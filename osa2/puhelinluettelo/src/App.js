be6536875259530bc9a23b296bdc540263523604
thyvonen <timo.hyvonen@gmail.com>
2021-05-10 08:26:37 +0300

    Add files via upload

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+    }
+  
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.findIndex(person => person.name === newName) === -1 ) {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(personObject))
+      setNewName(``)
+      setNewNumber(``)
+    }
+    else {
+      window.alert(`${newName} on jo puhelinluettelossa`);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.name}> {person.name} {person.number}</li>
+          )}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
